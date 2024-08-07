6069efd644112c12a186094c214791766a100be6
abdulissa21 <156181757+abdulissa21@users.noreply.github.com>
2024-08-07 16:26:34 +0100

    topics-router.js

---
+++ routes/topics-router.js
@@ -0,0 +1,6 @@
+const topicsRouter = require("express").Router();
+const { getTopics, postTopic } = require("../controllers/topics.controllers");
+
+topicsRouter.route("/").get(getTopics).post(postTopic);
+
+module.exports = { topicsRouter };
