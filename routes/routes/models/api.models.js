0841bd97f127585a677f0c198951461f6b522122
abdulissa21 <156181757+abdulissa21@users.noreply.github.com>
2024-08-07 16:25:14 +0100

    Delete routes directory

--- routes/routes/models/api.models.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs/promises");
-
-function fetchApiInfo() {
-	return fs.readFile("./endpoints.json", "utf-8", (err, fileContent) => {
-		return fileContent;
-	});
-}
-
-module.exports = { fetchApiInfo };
--- routes/topics-router.js
+++
@@ -1,6 +0,0 @@
-const topicsRouter = require("express").Router();
-const { getTopics, postTopic } = require("../controllers/topics.controllers");
-
-topicsRouter.route("/").get(getTopics).post(postTopic);
-
-module.exports = { topicsRouter };
